a7fd1421115b50829b57f14110a40e2d77ee5011
Stiffcliff <55393295+Stiffcliff@users.noreply.github.com>
2022-01-25 15:48:11 +0000

    modified frontend home

--- inc/frontendhome.php
+++ inc/frontendhome.php
@@ -1,16 +1,9 @@
 <?php
 include("dbconnect.php");
 
-$sql = "SELECT * FROM events";
-
-$rows = array();
-$result = $db->query($sql);
-while ($row = $result->fetch_array()) {
-    $rows[] = $row;
-}
-
-  header('Content-type: application/json');
-  echo json_encode($rows);
+<head>
+<title>h1 tags</title>
+</head>
 
 
 php?>
