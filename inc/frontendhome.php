6d68ebb001ba1b9199a91143292cb0a15a5657a5
Andrej Kocak <andrej.kocak1@gmail.com>
2022-01-26 09:46:58 +0100

    First draft of homepage

--- inc/frontendhome.php
+++ inc/frontendhome.php
@@ -1,8 +1,90 @@
 <?php  
 ?>
-
-<h1>Welcome to frontend home page</h1>
-
+<html>
+<head>
+    <title>Sustainable Dundee</title>
+    <meta charset="UTF-8">
+    <meta name="robots" value="noindex,follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="row">
+    <div class="col d-flex justify-content-center">
+        <h1>Sustainable Dundee</h1>
+    </div>
+</div>
+<div class="row mt-5">
+    <div class="col d-flex justify-content-center">
+        <h2>Intro text about site</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col d-flex mt-2 px-5">
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+    </div>
+</div>
+<div class="row mt-3 px-5">
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+</div>
+<div class="row mt-3 px-5">
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+    <div class="col">
+        <img src="dummy-image.jpg" height="200" width="200">
+        <h3>First 6 goals</h3>
+    </div>
+</div>
+</body>
+<footer>
+    <h3>Footer</h3>
+</footer>
+</html>
 <?php
 ?>
 
