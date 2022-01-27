9d6c2b2689f0210355a6f3ecda5d38a133448e43
Stiffcliff <55393295+Stiffcliff@users.noreply.github.com>
2022-01-27 17:21:09 +0000

    bcknd

--- inc/backendhome.php
+++ inc/backendhome.php
@@ -2,35 +2,6 @@
 include("dbconnect.php");  
 ?>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
-</script>
-
-<script>
-$(document).ready(function(){
-    $('#purpose').on('change', function () {
-    if(this.value === "1"){
-        $("#business").show();
-    } else {
-        $("#business").hide();
-    }
-});
-});
-</script>
-
-<body>
-<select id='purpose'>
-<option value="0">Personal use</option>
-<option value="1">Business use</option>
-<option value="2">Passing on to a client</option>
-</select>
-<div style='display:none;' id='business'>Business Name<br/>&nbsp;
-<br/>&nbsp;
-    <h1>business</h1>
-    <input type='text' class='text' name='business' value size='20' />
-    <br/>
-</div>
-</body>
-
 <form name="eventForm" method="post">
     <div class="container-fluid" id="eventContainer">
         <div class="row justify-content-center">
