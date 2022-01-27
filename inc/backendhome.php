d7f812f651b9f6a9023468cbee95f6f070274903
Stiffcliff <55393295+Stiffcliff@users.noreply.github.com>
2022-01-27 16:43:39 +0000

    backend page changes

--- inc/backendhome.php
+++ inc/backendhome.php
@@ -8,20 +8,20 @@ include("dbconnect.php");
 <script>
 $(document).ready(function(){
     $('#choice').on('change', function() {
-      if ( this.value == 'add')
+      if ( this.value == '0')
       
       {
         $("#add").show();
         $("#edit").hide();
         $("#remove").hide();
       }
-      if ( this.value == 'edit')
+      if ( this.value == '1')
       {
         $("#add").hide();
         $("#edit").show();
         $("#remove").hide();
       }
-      if ( this.value == 'remove')
+      if ( this.value == '2')
       {
         $("#add").hide();
         $("#edit").hide();
@@ -40,31 +40,39 @@ $(document).ready(function(){
 
 <h1>Welcome to backend home page</h1>
 
-<label for="cars">Add, edit or remove:</label>
+<label for="choice">Add, edit or remove:</label>
+
 <select name="choice" id="choice">
-<option value="add">add</option>
-<option value="edit">edit</option>
-<option value="remove">remove</option>
+<option value="0">add</option>
+<option value="1">edit</option>
+<option value="2">remove</option>
 </select>
+
 <br><br>
 
+<body>
 <div style='display:none;' id='add'>Add<br/>&nbsp;
+
 <br/>&nbsp;
+    <h1>add</h1>
     <input type='text' class='text' name='add' value size='20' />
     <br/>
 </div>
 
 <div style='display:none;' id='edit'>Edit<br/>&nbsp;
 <br/>&nbsp;
+    <h1>edit</h1>
     <input type='text' class='text' name='add' value size='20' />
     <br/>
 </div>
 
 <div style='display:none;' id='remove'>Remove<br/>&nbsp;
 <br/>&nbsp;
+    <h1>remove</h1>
     <input type='text' class='text' name='add' value size='20' />
     <br/>
 </div>
+</body>
 
 <?php
 ?>
