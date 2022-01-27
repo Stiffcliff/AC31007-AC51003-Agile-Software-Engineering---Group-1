5c7f30b3f4fad3ebce4e9ecfbf9ea7714c001d7d
Andrej Kocak <andrej.kocak1@gmail.com>
2022-01-27 19:21:20 +0100

    Resolving double submit of form 8

--- inc/backendhome.php
+++ inc/backendhome.php
@@ -2,7 +2,7 @@
 include("dbconnect.php");  
 ?>
 
-<form action="/handleForm.php" method="post">
+<form method="post">
     <div class="container-fluid" id="eventContainer">
         <div class="row justify-content-center">
             <div class="row">
@@ -34,4 +34,23 @@ include("dbconnect.php");
 </form>
 
 <?php
+    include "dbconnect.php";
+
+    echo "here";
+    echo $_POST('submit') !== null;
+    echo $_POST['eventName'];
+
+    if ( isset( $_POST['Submit'] ) ) {
+        echo "here";
+        $EventName = $_POST['eventName'];
+        $EventLocation = $_POST['eventLocation'];
+        $EventContact = $_POST['eventContact'];
+        $EventDescription = $_POST['eventDescription'];
+
+        $fullInsert = "INSERT INTO `nopovertyevents` (`EventID`, `EventName`, `EventLocation`, `EventContact`, `EventDescription`) VALUES (NULL,'$EventName','$EventLocation','$EventContact','$EventDescription')";
+        echo $fullInsert;
+        $db->query($fullInsert);
+
+        readfile("backendhome.php");
+    }
 ?>
