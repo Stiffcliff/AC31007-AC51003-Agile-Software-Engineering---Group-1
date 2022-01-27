b3a0ea07f3ea81bf00df8b6aa07294b295d3d43e
Andrej Kocak <andrej.kocak1@gmail.com>
2022-01-27 17:57:00 +0100

    Backend adding record to db

--- inc/backendhome.php
+++ inc/backendhome.php
@@ -31,5 +31,53 @@ $(document).ready(function(){
 </div>
 </body>
 
+<form name="eventForm" method="post">
+    <div class="container-fluid" id="eventContainer">
+        <div class="row justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <input type="text" id="eventName" name="eventName" class="form-control" placeholder="Name" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="text" id="eventLocation" name="eventLocation" class="form-control" placeholder="Location" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="text" id="eventContact" name="eventContact" class="form-control" placeholder="Contact" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="text" id="eventDescription" name="eventDescription" class="form-control" placeholder="Description" required>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col text-center">
+        <input type="submit" value="Submit">
+<!--        <button type="submit" name="insert" class="btn btn-secondary btn-lg">Add Record</button>-->
+    </div>
+</form>
+
+<?php
+include "dbconnect.php";
+
+echo "here";
+$EventName = $_POST['eventName'];
+$EventLocation = $_POST['eventLocation'];
+$EventContact = $_POST['eventContact'];
+$EventDescription = $_POST['eventDescription'];
+
+//`HolidayID`,`Name`,`LocationID`,`SeasonID`,`Availability`, `Price`, `TransportID`, `Description`, `Requirements`, `Rating`
+
+$fullInsert = "INSERT INTO nopovertyevents (EventName, EventLocation, EventContact, EventDescription) VALUES ('$EventName','$EventLocation','$EventContact','$EventDescription')";
+
+$execute = mysql_query($fullInsert);
+
+?>
+
 <?php
 ?>
