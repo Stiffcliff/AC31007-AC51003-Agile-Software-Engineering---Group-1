f99164fdb88e80e3531a0f393e7472faccecd835
Stiffcliff <55393295+Stiffcliff@users.noreply.github.com>
2022-01-28 11:34:52 +0000

    test

--- inc/frontendhome.php
+++ inc/frontendhome.php
@@ -7,7 +7,7 @@ define('INCLUDE_DIR', dirname(__FILE__) . '/resources/');
     <meta charset="UTF-8">
     <meta name="robots" value="noindex,follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css"/>
+    <link rel="stylesheet" href="/style.css"/>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
