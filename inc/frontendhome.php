fb3f18508e6a480523fb54de14f989cd2808b3d9
JasonBeesla <43534718+JasonBeesla@users.noreply.github.com>
2022-01-28 11:00:23 +0000

    linked first image to page

--- inc/frontendhome.php
+++ inc/frontendhome.php
@@ -35,7 +35,7 @@ define('INCLUDE_DIR', dirname(__FILE__) . '/resources/');
 <div class="main">
 	
     
-	<a href = "https://gitcdn.link/cdn/Stiffcliff/AC31007-AC51003-Agile-Software-Engineering---Group-1/main/inc/goal1nopoverty.php">
+	<a href = "goal1nopoverty.php">
         <img src="https://raw.githubusercontent.com/Stiffcliff/AC31007-AC51003-Agile-Software-Engineering---Group-1/main/inc/resources/no_poverty.png"height="150" width="150" alt = "No Poverty">
 	</a>
 <a href = "google.com">
