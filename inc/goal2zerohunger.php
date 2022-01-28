0a8e7e5b448b72371ff76f0cd8a5700446d8e2a3
JasonBeesla <43534718+JasonBeesla@users.noreply.github.com>
2022-01-28 12:11:53 +0000

    Added stylesheet to all the goal pages

--- inc/goal2zerohunger.php
+++ inc/goal2zerohunger.php
@@ -7,6 +7,7 @@
     <meta name="robots" value="noindex,follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="https://gitcdn.link/cdn/Stiffcliff/AC31007-AC51003-Agile-Software-Engineering---Group-1/main/inc/style.css"/>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
@@ -21,7 +22,6 @@
     </nav>
 </head>
 <body>
-<hr>
 <div class="row mt-5">
     <div class="col d-flex justify-content-center">
         <h2>Making Dundee A Better Place</h2>
