00c486f2c4dbb0c8ad5b603e2ca030aa53d5173b
Stiffcliff <55393295+Stiffcliff@users.noreply.github.com>
2022-01-28 11:59:18 +0000

    table display test

--- inc/goal1nopoverty.php
+++ inc/goal1nopoverty.php
@@ -1,4 +1,5 @@
 <?php
+include("dbconnect.php");  
 ?>
 
 <html>
@@ -51,5 +52,19 @@
 <hr>
 </body>
 </html>
+
 <?php
+$sql = "SELECT * FROM nopovertyevents";
+
+$result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
+echo "<br>";
+echo "<table border='1'>";
+while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
+    echo "<tr>";
+    foreach ($row as $field => $value) { // I you want you can right this line like this: foreach($row as $value) {
+        echo "<td>" . $value . "</td>"; // I just did not use "htmlspecialchars()" function. 
+    }
+    echo "</tr>";
+}
+echo "</table>";
 ?>
