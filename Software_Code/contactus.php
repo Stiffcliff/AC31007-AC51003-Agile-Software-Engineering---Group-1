fd7b7189089582e2464b428714e04fb7fc3822f7
JasonBeesla <43534718+JasonBeesla@users.noreply.github.com>
2022-02-01 16:46:57 +0000

    Created generic html code for the contact us page

---
+++ Software_Code/contactus.php
@@ -0,0 +1,17 @@
+<html>
+<h2>Make a suggestion of a sustainable example in Dundee!</h2>
+<form action "contact.php" method = "post">
+
+<p><label>Name of Place:</label><input name = "name" type="text" /></p>
+<p><label>Location:</label><input name = "location" type = "text" /></p>
+<p><label>Contact Email of Location:</label><input name = "email" type = "text" /></p>
+<p><label>Phone Number of Location:</label><input name = "phoneNumber" type = "text" /></p>
+<p><label>Website:</label><input name = "website" type = "text" /></p>
+<p><label>Description: </label><input name = "description" type = "text" /></p>
+
+<p><input type = "submit" value = "Send" /></p>
+</form>
+
+<?php
+?>
+</html>
