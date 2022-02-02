17b133bc27445403a5e40a986c9ee044e5de2c47
JasonBeesla <43534718+JasonBeesla@users.noreply.github.com>
2022-02-02 13:31:22 +0000

    Contact us page has been configured, and email api library added to folder

---
+++ Software_Code/sendgrid-php/vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '55b93208b406bd3b57edc620ae24ded7c257d329',
+        'name' => 'sendgrid/sendgrid',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'sendgrid/php-http-client' => array(
+            'pretty_version' => '3.14.1',
+            'version' => '3.14.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sendgrid/php-http-client',
+            'aliases' => array(),
+            'reference' => '457bfeb199e01cf7a8b96a60789306d7ea16307e',
+            'dev_requirement' => false,
+        ),
+        'sendgrid/sendgrid' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '55b93208b406bd3b57edc620ae24ded7c257d329',
+            'dev_requirement' => false,
+        ),
+        'sendgrid/sendgrid-php' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'starkbank/ecdsa' => array(
+            'pretty_version' => '0.0.5',
+            'version' => '0.0.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../starkbank/ecdsa',
+            'aliases' => array(),
+            'reference' => '484bedac47bac4012dc73df91da221f0a66845cb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
