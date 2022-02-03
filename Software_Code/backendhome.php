6208980022062fcfc13fa40c6b57e84e8f64f0e9
Stiffcliff <55393295+Stiffcliff@users.noreply.github.com>
2022-02-03 20:51:23 +0000

    added comments to the backend page

--- Software_Code/backendhome.php
+++ Software_Code/backendhome.php
@@ -1,10 +1,11 @@
 <?php
-include("dbconnect.php");  
+include("dbconnect.php"); //including database connect script 
 ?>
 
 
 <style>
-#addEverything {
+
+#addEverything { /* styling for add division */
   width: 100%;
   padding: 50px 0;
   text-align: center;
@@ -12,7 +13,7 @@ include("dbconnect.php");
   margin-top: 20px;
 }
 
-#editEverything {
+#editEverything { /* styling for edit division */
   width: 100%;
   padding: 50px 0;
   text-align: center;
@@ -20,7 +21,7 @@ include("dbconnect.php");
   margin-top: 20px;
 }
 
-#removeEverything {
+#removeEverything { /* styling for remove division */
   width: 100%;
   padding: 50px 0;
   text-align: center;
@@ -29,21 +30,22 @@ include("dbconnect.php");
 }
 </style>
 
-<select name="dropdown" id="dropdown" onchange=choiceFunction()>
+<!--dropdown box to select option-->
+<select name="dropdown" id="dropdown" onchange=choiceFunction()> <!--runs function on dropbox change-->
     <option hidden disabled selected value> -- select an option -- </option>
     <option value="1">Add</option>
     <option value="2">Edit</option>
     <option value="3">Remove</option>
 </select>
 
-
-    <div id='addEverything'>
-        <form method="post" id="addForm">
+    <!--divison for add function-->
+    <div id='addEverything'> <!--runs function on dropbox change-->
+        <form method="post" id="addForm"> <!--form running post method for passing to add php script-->
             <div class="container-fluid" id="eventContainer">
                 <div class="row justify-content-center">
                     <div class="row">
                         <div class="col">
-                            <select name="eventTable" id="eventTable">
+                            <select name="eventTable" id="eventTable"> <!--Dropdown select for table-->
                                 <option hidden disabled selected value> Select table name</option>
                                 <option value="nopovertyevents">nopovertyevents - Goal 1</option>
                                 <option value="zerohungerevents">zerohungerevents - Goal 2</option>
@@ -65,43 +67,44 @@ include("dbconnect.php");
                             </select>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col">
+                    <div class="row"> 
+                        <div class="col"> <!--Event name input box-->
                             <input type="text" id="eventName" name="eventName" class="form-control" placeholder="Name" required>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col"> <!--Event location input box-->
                             <input type="text" id="eventLocation" name="eventLocation" class="form-control" placeholder="Location" required>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col"> <!--Event contact input box-->
                             <input type="text" id="eventContact" name="eventContact" class="form-control" placeholder="Contact" required>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col"><!--Event description input box-->
                             <input type="text" id="eventDescription" name="eventDescription" class="form-control" placeholder="Description" required>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col text-center">
+            <div class="col text-center"> <!--submit button for add form-->
                 <input type="submit" name="submit" value="Submit">
         <!--        <button type="submit" name="insert" class="btn btn-secondary btn-lg">Add Record</button>-->
             </div>
         </form>
     </div>
 
+<!--divison for edit function-->    
 <div id='editEverything'>
 <h1>Edit</h1>
-    <form method="post" id="editForm">
+    <form method="post" id="editForm"> <!--form for edit function-->
         <div class="container-fluid" id="eventContainer">
             <div class="row justify-content-center">
                 <div class="row">
                     <div class="col">
-                        <select name="eventTable" id="eventTable">
+                        <select name="eventTable" id="eventTable"> <!--dropdown box for choosing table to edit-->
                             <option hidden disabled selected value> Select table name</option>
                             <option value="nopovertyevents">nopovertyevents - Goal 1</option>
                             <option value="zerohungerevents">zerohungerevents - Goal 2</option>
@@ -124,46 +127,46 @@ include("dbconnect.php");
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col">
+                    <div class="col"> <!--Event ID input box-->
                         <input type="text" id="eventId" name="eventId" class="form-control" placeholder="Event ID" required>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col"> <!--Event name input box-->
                             <input type="text" id="eventName" name="eventName" class="form-control" placeholder="Name" required>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col"> <!--Event location input box-->
                             <input type="text" id="eventLocation" name="eventLocation" class="form-control" placeholder="Location" required>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col"> <!--Event contact input box-->
                             <input type="text" id="eventContact" name="eventContact" class="form-control" placeholder="Contact" required>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col"> <!--Event description input box-->
                             <input type="text" id="eventDescription" name="eventDescription" class="form-control" placeholder="Description" required>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col text-center">
+        <div class="col text-center"> <!--Submit button for edit-->
             <input type="submit" name="submitEdit" value="Submit edit">
         </div>
     </form>
 </div>
 
-<div id='removeEverything'>
+<div id='removeEverything'> <!--remove event division-->
     <h1>Remove</h1>
-    <form method="post" id="removeForm">
+    <form method="post" id="removeForm"> <!--remove form-->
         <div class="container-fluid" id="eventContainer">
             <div class="row justify-content-center">
                 <div class="row">
                     <div class="col">
-                        <select name="eventTable" id="eventTable">
+                        <select name="eventTable" id="eventTable"> <!--dropdown box for selecting-->
                             <option hidden disabled selected value> Select table name</option>
                             <option value="nopovertyevents">nopovertyevents - Goal 1</option>
                             <option value="zerohungerevents">zerohungerevents - Goal 2</option>
@@ -186,20 +189,20 @@ include("dbconnect.php");
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col">
+                    <div class="col"> <!--event id input box-->
                         <input type="text" id="eventId" name="eventId" class="form-control" placeholder="Event ID" required>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col text-center">
+        <div class="col text-center"> <!--delete submit button-->
             <input type="submit" name="submitRemove" value="Submit delete">
         </div>
     </form>
 </div>
 
-<form method="post" id="displayForm">
-    <select name="tabledrop" id="tabledrop">
+<form method="post" id="displayForm"> <!--form for displaying the table on the page-->
+    <select name="tabledrop" id="tabledrop"> <!--dropdown box listing tables-->
         <option hidden disabled selected value> Select a table </option>
         <option value="nopovertyevents">nopovertyevents - Goal 1</option>
         <option value="zerohungerevents">zerohungerevents - Goal 2</option>
@@ -219,7 +222,7 @@ include("dbconnect.php");
         <option value="peaceevents">peaceevents - Goal 16</option>
         <option value="partnerevents">partnerevents - Goal 17</option>
     </select>
-    <div class="col text-center">
+    <div class="col text-center"> <!--submit button for displaying table-->
         <input type="submit" name="submitDisplay" value="Display table">
         <!--        <button type="submit" name="insert" class="btn btn-secondary btn-lg">Add Record</button>-->
     </div>
@@ -229,45 +232,46 @@ include("dbconnect.php");
 </div>
 
 <?php
-    include "dbconnect.php";
+    include "dbconnect.php"; //connecting doconnect script
 
-    if ( isset( $_POST['submit'] ) ) {
+    if ( isset( $_POST['submit'] ) ) { //post form script for add functionality
 
         $EventTable = $_POST['eventTable'];
         $EventName = $_POST['eventName'];
         $EventLocation = $_POST['eventLocation'];
         $EventContact = $_POST['eventContact'];
-        $EventDescription = $_POST['eventDescription'];
+        $EventDescription = $_POST['eventDescription']; //setting the variables in php passed from the page
 
         $fullInsert = "INSERT INTO `$EventTable` (`EventID`, `EventName`, `EventLocation`, `EventContact`, `EventDescription`) VALUES (NULL,'$EventName','$EventLocation','$EventContact','$EventDescription')";
-
+        //writing sql statement to a variable
         echo $fullInsert;
 
-        $db->query($fullInsert);
+        $db->query($fullInsert); //running the query on the database
 
-        readfile("backendhome.php");
+        readfile("backendhome.php"); //refreshing the page
     }
 ?>
 
 <?php
 include "dbconnect.php";
 
-if ( isset( $_POST['submitRemove'] ) ) {
+if ( isset( $_POST['submitRemove'] ) ) {//post form script for remove functionality
 
     $EventTable = $_POST['eventTable'];
     $EventId = intval($_POST['eventId']);
     $EventName = $_POST['eventName'];
     $EventLocation = $_POST['eventLocation'];
     $EventContact = $_POST['eventContact'];
-    $EventDescription = $_POST['eventDescription'];
+    $EventDescription = $_POST['eventDescription']; //setting the variables in php passed from the page
 
-    $fullDelete = "DELETE FROM `$EventTable` WHERE EventID=$EventId";
-    $db->query($fullDelete);
+    $fullDelete = "DELETE FROM `$EventTable` WHERE EventID=$EventId"; //writing sql statement to a variable
+    
+    $db->query($fullDelete);  //running the query on the database
 
     echo $fullDelete;
 
 
-    readfile("backendhome.php");
+    readfile("backendhome.php"); //refreshing the page
 }
 ?>
 
@@ -281,67 +285,69 @@ if ( isset( $_POST['submitEdit'] ) ) {
     $EventName = $_POST['eventName'];
     $EventLocation = $_POST['eventLocation'];
     $EventContact = $_POST['eventContact'];
-    $EventDescription = $_POST['eventDescription'];
+    $EventDescription = $_POST['eventDescription']; //setting the variables in php passed from the page
 
-    $fullDelete = "DELETE FROM `$EventTable` WHERE EventID=$EventId";
+    $fullDelete = "DELETE FROM `$EventTable` WHERE EventID=$EventId"; //writing delete sql statement to a variable
     
     $fullInsert = "INSERT INTO `$EventTable` (`EventID`, `EventName`, `EventLocation`, `EventContact`, `EventDescription`) VALUES ('$EventID','$EventName','$EventLocation','$EventContact','$EventDescription')";
-    
-    $db->query($fullDelete);
-    $db->query($fullInsert);
+    //writing add sql statement to a variable
 
-    readfile("backendhome.php");
+    $db->query($fullDelete);
+    $db->query($fullInsert); //running both sql statements (could need multi query)
+
+    readfile("backendhome.php");//refreshing the page
 }
 ?>
 
-<script>
-    document.getElementById("addForm").reset();
+<script> //start of javascript 
 
-    var addEverythingControl = document.getElementById("addEverything");
+    document.getElementById("addForm").reset(); //resets add form
+
+    var addEverythingControl = document.getElementById("addEverything"); 
     var editEverythingControl = document.getElementById("editEverything");
-    var removeEverythingControl = document.getElementById("removeEverything");
+    var removeEverythingControl = document.getElementById("removeEverything"); //assigning add/edit/remove to variables
 
     addEverythingControl.style.display = "none";
     editEverythingControl.style.display = "none";
-    removeEverythingControl.style.display = "none";
+    removeEverythingControl.style.display = "none"; //hiding sections on page start
 
     
     function choiceFunction() {
         addEverythingControl = document.getElementById("addEverything");
         editEverythingControl = document.getElementById("editEverything");
-        removeEverythingControl = document.getElementById("removeEverything");
+        removeEverythingControl = document.getElementById("removeEverything"); //assigning add/edit/remove to variables
 
-        var dropdownID = document.getElementById("dropdown");
-        var dropdownvalue = dropdownID.value;
+        var dropdownID = document.getElementById("dropdown"); //setting dropdown box to variable
+        var dropdownvalue = dropdownID.value; //setting value of selected dropdown to variable
 
         if (dropdownvalue == "1") {
             addEverythingControl.style.display = "block";
             editEverythingControl.style.display = "none";
-            removeEverythingControl.style.display = "none";
+            removeEverythingControl.style.display = "none"; //showing add section
         }
         
         if (dropdownvalue == "2") {
             addEverythingControl.style.display = "none";
             editEverythingControl.style.display = "block";
-            removeEverythingControl.style.display = "none";
+            removeEverythingControl.style.display = "none";//showing edit section
         }
         if (dropdownvalue == "3") {
             addEverythingControl.style.display = "none";
             editEverythingControl.style.display = "none";
-            removeEverythingControl.style.display = "block";
+            removeEverythingControl.style.display = "block";//showing remove section
         }
     }
 
     function tableFunction(){
-        var tableID = document.getElementById("tabledrop");
-        var tablevalue = tableID.options[tableID.selectedIndex].text;
+        var tableID = document.getElementById("tabledrop"); //sets tableID to dropdown box element
+        var tablevalue = tableID.options[tableID.selectedIndex].text; //sets value of dropdown to variable
         console.log(tablevalue);
         //need to pass tablevalue to php and mysql command to show the selected table - tablevalue="nopovertyevents" etc
     }
 
     function addFunction(){
-        var addID = document.getElementById("adddrop");
-        var addvalue = addID.options[addID.selectedIndex].text;
+        var addID = document.getElementById("adddrop"); //sets tableID to dropdown box element
+        var addvalue = addID.options[addID.selectedIndex].text; //sets value of dropdown to variable
         console.log(addvalue);
         //need to pass addvalue to php and mysql command to insert into selected table - addvalue="nopovertyevents" et
     }
@@ -351,15 +357,16 @@ if ( isset( $_POST['submitEdit'] ) ) {
 </script>
 
 <?php
-if ( isset( $_POST['submitDisplay'] ) ) {
+if ( isset( $_POST['submitDisplay'] ) ) { //submit script for table display
     $tableName = $_POST['tabledrop'];
     $sql = "SELECT EventID,EventName,EventLocation,EventContact,EventDescription FROM `$tableName`";
-    $result = $db->query($sql);
+    //setting the sql statement to a variable
+    $result = $db->query($sql); //running query
 
     echo "<table class='table'> 
             <thead>
                 <tr>
-                    <th scope='col'>ID</th>
+                    <th scope='col'>ID</th> 
                     <th scope='col'>Name</th>
                     <th scope='col'>Location</th>
                     <th scope='col'>Contact</th>
@@ -367,6 +374,7 @@ if ( isset( $_POST['submitDisplay'] ) ) {
                 </tr>
             </thead>
             <tbody>";
+    //displaying table
 
     if ($result->num_rows > 0) {
         // output data of each row
@@ -388,7 +396,7 @@ if ( isset( $_POST['submitDisplay'] ) ) {
     echo "</tbody>";
     echo "</table>";
 
-    $db->close();
+    $db->close(); //close connection to database
 }
 
 ?>
