a52c4d71d79632c11faf7f3d2d665539e7da43a8
leiaainscough <55392194+leiaainscough@users.noreply.github.com>
2022-02-04 13:49:43 +0000

    Update goal14lifebelowwater.php

--- Software_Code/goal14lifebelowwater.php
+++ Software_Code/goal14lifebelowwater.php
@@ -12,16 +12,19 @@ include("dbconnect.php");
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+    //nav links
     <nav class = "nav">
-    <ul>
-        <li><a href = "#"><img src="https://raw.githubusercontent.com/Stiffcliff/AC31007-AC51003-Agile-Software-Engineering---Group-1/main/Software_Code/resources/sustainable800.png" height="56.3" alt="Sustainable dundee logo"></a></li>
-        <li style= "float: right"><a href="#contact">Contact Us</a></li>
-    </ul>    
+        <ul>
+        <li><a href = "frontendhome"><img src="https://raw.githubusercontent.com/Stiffcliff/AC31007-AC51003-Agile-Software-Engineering---Group-1/main/Software_Code/resources/sustainable800.png" height="56.3" alt="Sustainable dundee logo"></a></li>
+        <li style= "float: right"><a href="contactus">Contact Us</a></li>
+	<li style= "float: right"><a href="map">Interactive Map</a></li>
+    </ul>  
     </nav>
 </head>
 <body>
 <div class="row mt-5">
     <div class="col d-flex justify-content-center">
+        //header
         <br>
         <h2>Making Dundee A Better Place</h2>
         <br>
@@ -30,6 +33,7 @@ include("dbconnect.php");
 <hr>
 <div class="row">
     <div class="col d-flex mt-2 px-5">
+        //more info about chosen goal
         <h3>Goal 14: Life below Water</h3>
         <p>
           Conserve and sustainably use the oceans, seas and marine resources for sustainable development.
@@ -65,6 +69,7 @@ include("dbconnect.php");
 <h4>Get involved locally</h4>
 <br>
 
+    //read local items from DB
 <?php
 $sql = "SELECT EventID,EventName,EventLocation,EventContact,EventDescription FROM lifebelowevents";
 $result = $db->query($sql);
