184523fa762fe32b8baed8f335514400a4975a9e
leiaainscough <55392194+leiaainscough@users.noreply.github.com>
2022-02-04 13:38:27 +0000

    Update goal8decentworkandeconomicgrowth.php

--- Software_Code/goal8decentworkandeconomicgrowth.php
+++ Software_Code/goal8decentworkandeconomicgrowth.php
@@ -10,17 +10,19 @@ include("dbconnect.php");
     <link rel="stylesheet" href="https://gitcdn.link/cdn/Stiffcliff/AC31007-AC51003-Agile-Software-Engineering---Group-1/main/Software_Code/style.css"/>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+//nav links
     <nav class = "nav">
-    <ul>
-        <li><a href = "#"><img src="https://raw.githubusercontent.com/Stiffcliff/AC31007-AC51003-Agile-Software-Engineering---Group-1/main/Software_Code/resources/sustainable800.png" height="56.3" alt="Sustainable dundee logo"></a></li>
-        <li style= "float: right"><a href="#contact">Contact Us</a></li>
-    </ul>    
+        <ul>
+        <li><a href = "frontendhome"><img src="https://raw.githubusercontent.com/Stiffcliff/AC31007-AC51003-Agile-Software-Engineering---Group-1/main/Software_Code/resources/sustainable800.png" height="56.3" alt="Sustainable dundee logo"></a></li>
+        <li style= "float: right"><a href="contactus">Contact Us</a></li>
+	<li style= "float: right"><a href="map">Interactive Map</a></li>
+    </ul>  
     </nav>
 </head>
 <body>
 <div class="row mt-5">
     <div class="col d-flex justify-content-center">
+        //header
         <br>
         <h2>Making Dundee A Better Place</h2>
         <br>
@@ -29,6 +31,7 @@ include("dbconnect.php");
 <hr>
 <div class="row">
     <div class="col d-flex mt-2 px-5">
+        //more info about chosen goal
         <h3>Goal 8: Decent Work and Economic Growth (Green economy, sustainable tourism)</h3>
         <p>
           Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all
@@ -59,7 +62,7 @@ include("dbconnect.php");
 </body>
 <h4>Get involved locally</h4>
 <br>
-
+//read in local items from DB
 <?php
 $sql = "SELECT EventID,EventName,EventLocation,EventContact,EventDescription FROM ecogrowevents";
 $result = $db->query($sql);
